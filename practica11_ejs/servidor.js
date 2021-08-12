856c4f677d7aececc24569ad313ea56a40f39111
jmancu01 <73651528+jmancu01@users.noreply.github.com>
2021-08-12 17:19:20 -0300

    practica 11_ejs

---
+++ practica11_ejs/servidor.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import path from 'path'
+import Products from '../practica8/functions.js'
+
+const app = express()
+const port = 8080
+
+const server = app.listen(port, ()=>
+    console.log('escuchando en el puerto 8080')
+)
+
+const publicFolderPath = path.resolve('../public')
+app.use(express.static(publicFolderPath))
+
+const viewsFolderPath = path.resolve( '../practica11_ejs/views')
+
+app.set('view engine', 'ejs')
+app.set('views', viewsFolderPath)
+
+app.get('/', (req, res) => {
+    const products = new Products()
+    const arrayProductos = products.get()
+
+    const datosDinamicos = {
+        nombre: 'Juan',
+        apellido: 'Mancuso',
+        arrayProductos: arrayProductos,
+    }
+
+    res.render('main', datosDinamicos)
+})
+app.get('/guardar', (req, res) =>{
+    
+    const products = new Products(req.query.nombre, parseInt(req.query.precio))
+  
+    const data = products.guardar()
+
+    res.json(
+        {
+            data
+        }
+    )
+})
