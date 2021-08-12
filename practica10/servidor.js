d2349328fc2d031006805f864c71fb4aabe7fab5
jmancu01 <73651528+jmancu01@users.noreply.github.com>
2021-08-12 14:25:23 -0300

    forms handlebars

--- practica10/servidor.js
+++ practica10/servidor.js
@@ -24,11 +24,24 @@ app.engine('handlebars', handlebars({
 app.get('/', (req, res) => {
     const products = new Products()
     const arrayProductos = products.get()
+
     const datosDinamicos = {
         nombre: 'Juan',
         apellido: 'Mancuso',
-        arrayProductos: arrayProductos
+        arrayProductos: arrayProductos,
     }
 
     res.render('main', datosDinamicos)
+})
+app.get('/guardar', (req, res) =>{
+    
+    const products = new Products(req.query.nombre, parseInt(req.query.precio))
+    console.log(products)
+    const data = products.guardar()
+
+    res.json(
+        {
+            data
+        }
+    )
 })
